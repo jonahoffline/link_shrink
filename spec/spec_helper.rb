0a494c30568282053ab2ff485b941271c566dd9c
Jonah Ruiz <jonah@pixelhipsters.com>
2013-08-26 18:13:04 -0400

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,32 @@ require 'simplecov'
 require 'simplecov-gem-adapter'
 
 SimpleCov.start 'gem'
+require 'webmock/rspec'
+require 'vcr'
 require 'shared_examples'
 require 'link_shrink'
 
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = {
+    record: :new_episodes
+  }
+  c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = true
+end
+
 RSpec.configure do |config|
-  #config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.order = 'random'
 end
+
+#before :each do
+#  VCR.use_cassette 'google_url' do
+#    LinkShrink.shrink_url('http://www.google.com')
+#  end
+#end
+
+#vcr_options = { :cassette_name => 'google_url', :record => :new_episodes }
