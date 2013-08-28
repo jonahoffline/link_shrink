4353aba835e67d65d9c0ee631fee189a47417c6e
Jonah Ruiz <jonah@pixelhipsters.com>
2013-08-28 01:29:33 -0400

    Update config to set Google as fallback

--- lib/link_shrink/config.rb
+++ lib/link_shrink/config.rb
@@ -2,6 +2,14 @@ module LinkShrink
   module Config
     extend self
 
-    attr_accessor :api_key, :api
+    attr_accessor :api_key
+
+    def api=(api)
+      @api = api.new
+    end
+
+    def api
+      @api || LinkShrink::Shrinkers::Google.new
+    end
   end
 end
