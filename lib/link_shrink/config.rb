0e8ea89908d58b28d084a56cb72deca3983c8f41
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-06 05:27:52 -0400

    Update api setter method to read from const_get

--- lib/link_shrink/config.rb
+++ lib/link_shrink/config.rb
@@ -4,13 +4,19 @@ module LinkShrink
     # Configurable module for integrating with other URL APIs
     extend self
 
+    # Returns and Sets API key to be used
+    # @param api_key [String] API key to be used
     attr_accessor :api_key
 
-
+    # Sets API to be used
+    # @param api [String or Symbol] api to be used
     def api=(api)
-      @api = api.new
+      @api = LinkShrink::Shrinkers.const_get(api).new
     end
 
+    # Returns API used
+    # Unless an API is defined, it returns Google by default
+    # @return [LinkShrink::Shrinkers::Google] instance
     def api
       @api || LinkShrink::Shrinkers::Google.new
     end
