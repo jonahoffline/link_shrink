e79ba962d1f8c575ea6689a9f8446bf049bee17d
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-06 01:27:59 -0400

    Add documentation to TinyURL class

--- lib/link_shrink/shrinkers/tinyurl.rb
+++ lib/link_shrink/shrinkers/tinyurl.rb
@@ -3,18 +3,26 @@ module LinkShrink
     # @author Jonah Ruiz <jonah@pixelhipsters.com>
     # Implements Tiny URL Shortener API
     class TinyUrl < Base
+      # Returns URL base for API
+      # @return [String] api base url
       def base_url
         'http://tinyurl.com/api-create.php'
       end
 
+      # URL query parameters
+      # @return [String] query parameters to be used in request
       def api_query_parameter
         "?url=#{url}"
       end
 
+      # Returns Content-Type to be used in Request headers
+      # @return [String] text/plain as content-type
       def content_type
         'text/plain'
       end
 
+      # Returns full api url
+      # @return [String] full api url with query parameters
       def api_url
         base_url.concat api_query_parameter
       end
