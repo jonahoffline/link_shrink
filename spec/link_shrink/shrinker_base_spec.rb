da973a55263f6c708758ea82d96fba05024c1dc6
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-06 01:19:49 -0400

    Update base specs for 3 missing methods

--- spec/link_shrink/shrinker_base_spec.rb
+++ spec/link_shrink/shrinker_base_spec.rb
@@ -22,11 +22,33 @@ describe LinkShrink::Shrinkers::Base do
   end
 
   describe '#api_url' do
-    it 'raises error due to other methods not being overridden' do
-      expect{ link_shrink.base_url }.to raise_error
+    context 'when api_key is found' do
+      it 'returns base_url with api_query_parameters' do
+        link_shrink.stub(:sub_klass).and_return('Blahblah')
+        link_shrink.stub(:api_key).and_return('12345')
+        link_shrink.stub(:api_key?).and_return(true)
+
+        link_shrink.stub(:base_url).and_return('http://blah.io/api')
+        link_shrink.stub(:api_query_parameter).and_return('?key=12345')
+        expect(link_shrink.api_url).to eq('http://blah.io/api?key=12345')
+      end
+    end
+
+    context 'when api_key is not found' do
+      it 'returns base_url' do
+        link_shrink.stub(:sub_klass).and_return('Blahblah')
+        link_shrink.stub(:base_url).and_return('http://blah.io/api')
+        expect(link_shrink.api_url).to eq('http://blah.io/api')
+      end
     end
   end
-  #
+
+  describe '#generate_chart_url' do
+    it 'raises error due to other methods not being overridden' do
+      expect{ link_shrink.generate_chart_url('something') }.to raise_error
+    end
+  end
+
   describe '#api_key?' do
     it 'returns false when API is not found' do
       link_shrink.stub(:sub_klass).and_return('blahblah')
@@ -35,9 +57,20 @@ describe LinkShrink::Shrinkers::Base do
   end
 
   describe '#api_key' do
-    it 'returns the api key' do
-      link_shrink.stub(:sub_klass).and_return('blahblah')
-      expect(link_shrink.api_key).to eq(nil)
+    context 'when not found' do
+      it 'returns nil' do
+        link_shrink.stub(:sub_klass).and_return('Blah')
+        expect(link_shrink.api_key).to eq(nil)
+      end
+    end
+
+    context 'when found' do
+      it 'returns api_key' do
+        link_shrink.stub(:sub_klass).and_return('Blah')
+        #link_shrink.stub(:api).and_return('Blah')
+        ENV.store('BLAH_URL_KEY', '12345')
+        expect(link_shrink.api_key).to eq('12345')
+      end
     end
   end
 
@@ -45,6 +78,33 @@ describe LinkShrink::Shrinkers::Base do
     it 'cleans URL' do
       expect(link_shrink.sanitize_url("http://www.google.com/ "))
       .to eq("http://www.google.com/%20")
+    end
+  end
+
+  describe '#http_method' do
+    it 'returns :get' do
+      expect(link_shrink.http_method).to eq(:get)
+    end
+  end
+
+  describe '#content_type' do
+    it 'returns application/json by default' do
+      expect(link_shrink.content_type).to eq('application/json')
+    end
+  end
+
+  describe '#url' do
+    context 'when set' do
+      it 'returns url' do
+        link_shrink.url = 'http://www.google.com'
+        expect(link_shrink.url).to eq('http://www.google.com')
+      end
+    end
+
+    context 'when called without being set' do
+      it 'returns nil' do
+        expect(link_shrink.url).to eq(nil)
+      end
     end
   end
 end
