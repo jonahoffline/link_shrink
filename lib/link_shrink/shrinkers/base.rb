7cbddd6a7925e4a6f7f4d6cb4c3aa2a08e753947
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-08 07:48:52 -0400

    Add new mini-dsl for defining the JSON response structure to be parsed

--- lib/link_shrink/shrinkers/base.rb
+++ lib/link_shrink/shrinkers/base.rb
@@ -84,6 +84,28 @@ module LinkShrink
       def generate_chart_url(new_url, image_size = {})
         fail "#{__method__} not implemented"
       end
+
+      # Handles DSL definition of response structure to be parsed (JSON)
+      class << self
+        attr_accessor :collection_key, :url_key
+
+        # Helper method that yields into the other response structure methods
+        def response_options(&block)
+          yield
+        end
+
+        # Defines collection_key in response
+        # @param new_collection [String] collection_key
+        def collection(new_collection)
+          self.collection_key = new_collection
+        end
+
+        # Defines url_key in response
+        # @param new_url_key [String] url_key
+        def short_url(new_url_key)
+          self.url_key = new_url_key
+        end
+      end
     end
   end
 end
