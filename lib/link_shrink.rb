b4b530ff144caf3a96b5b73e404224b8f64875f4
Jonah Ruiz <jonah@pixelhipsters.com>
2013-09-08 07:55:42 -0400

    Add Owly API class to require from linkshrink module

--- lib/link_shrink.rb
+++ lib/link_shrink.rb
@@ -6,6 +6,7 @@ require 'link_shrink/shrinkers/base'
 require 'link_shrink/shrinkers/google'
 require 'link_shrink/shrinkers/tinyurl'
 require 'link_shrink/shrinkers/isgd'
+require 'link_shrink/shrinkers/owly'
 require 'link_shrink/config'
 
 # @author Jonah Ruiz <jonah@pixelhipsters.com>
