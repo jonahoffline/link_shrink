794692b3d7cbf415676a7043ab2eaa8ff5c38a2f
Jonah Ruiz <jonah@pixelhipsters.com>
2013-10-03 12:15:06 -0400

    Remove shared_examples related to qr_codes and json options

--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -12,11 +12,7 @@ shared_context 'shared_examples' do
   let(:link_shrink) { described_class }
   let(:url) { 'http://www.google.com' }
   let(:short_url) { "http://goo.gl/fbsS" }
-  let(:qrcode_url) { 'http://goo.gl/fbsS.qr' }
 
   let(:json_response) { fixture('response.json') }
-  let(:json_qr_code) { fixture('response_qr_code.json') }
-  let(:json_qr_code_custom) { fixture('response_qr_code_custom.json') }
   let(:parsed_json) { { "kind" => "urlshortener#url", "id" => "http://goo.gl/fbsS", "longUrl" => "http://www.google.com/" } }
-  let(:chart_url) { "http://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chld=H&chl=http://goo.gl/fbsS" }
 end
