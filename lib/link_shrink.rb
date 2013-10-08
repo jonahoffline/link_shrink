57e2c442cf293eb5a9ecf719c0ee67eeb66c8d19
Jonah Ruiz <jonah@pixelhipsters.com>
2013-10-08 12:37:01 -0400

    Refactor LinkShrink module and update documentation

--- lib/link_shrink.rb
+++ lib/link_shrink.rb
@@ -11,31 +11,17 @@ require 'link_shrink/shrinkers/owly'
 require 'link_shrink/config'
 
 # @author Jonah Ruiz <jonah@pixelhipsters.com>
-# Creates a short URL and QR codes
+# Creates a short URLs
 module LinkShrink
   extend self
   include LinkShrink::Request
-  # Returns a short URL or JSON response
-  #   example: shrink_url('http://www.wtf.com', { json: true, qr_code: true })
-  #   example: shrink_url('http://www.wtf.com', { qr_code: true })
+  # Returns a short URL
+  #   example: shrink_url('http://www.wtf.com')
   #
   # @param url [String] long URL to be shortened
-  # @param options [Hash] format to be returned
-  # @return [String] generated short URL or JSON response
-  def shrink_url(url, options = { json: false, qr_code: false })
-    process_request(url, options)
-  end
-
-  # Returns a QR code URL
-  #   example: generate_qr_code('http://www.wtf.com', { image_size: '300x300' })
-  #
-  # @param url [String] long URL to be shortened
-  # @param options [Hash] image_size: '300x300' for a custom size
-  # @return [String] QR code URL using default or custom size
-  def generate_qr_code(url, options = {})
-    new_url    = process_request(url, {})
-    image_size = options.fetch(:image_size, {})
-    Config.api.generate_chart_url(new_url, image_size)
+  # @return [String] generated short URL
+  def shrink_url(url)
+    process_request(url)
   end
 
   # Yield's to Config for options
